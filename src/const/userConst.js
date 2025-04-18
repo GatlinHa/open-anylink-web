10c47f4389ba1cc99c8da214d0f5402bfb52d3b0
bob <312777916@qq.com>
2025-04-18 10:02:58 +0800

    更新1.2.0版本号

--- src/const/userConst.js
+++ src/const/userConst.js
@@ -1,6 +1,6 @@
 export const CLIENT_TYPE = 2
 export const CLIENT_NAME = 'web'
-export const CLIENT_VERSION = '1.1.0'
+export const CLIENT_VERSION = '1.2.0'
 
 export const LEAVING_AFTER_DURATION = 5 * 60 * 1000
 export const LOGOUT_AFTER_DURATION = 8 * 60 * 60 * 1000
